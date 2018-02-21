d1f6eb18fb1eaffc9b45c8188044a4eb6a842df8
Martha <lupita.alvarado1996@gmail.com>
2018-02-21 13:52:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/marthaalvarado/priceschedulesviv/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.marthaalvarado.priceschedulesviv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
